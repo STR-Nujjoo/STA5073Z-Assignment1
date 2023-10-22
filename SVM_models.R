2e27b429a093a2630e133669fcd478902fd2481b
your user github username <your github email>
2023-10-22 03:42:56 +0200

    work in progress

--- SVM_models.R
+++ SVM_models.R
@@ -16,11 +16,13 @@ svm_bow_200 <- svm(Pid ~.,
 
 save(svm_bow_200, file = "Rdata/SVM_bow_200.Rdata")
 
-svm_bow_200_train_accuracy <- mean(predict(svm_bow_200, newdata = train_bow_200) == train_bow_200$Pid)
-svm_bow_200_test_accuracy <- mean(predict(svm_bow_200, newdata = test_bow_200) == test_bow_200$Pid)
+svm_bow_200_trainpred <- predict(svm_bow_200, newdata = train_bow_200)
+svm_bow_200_testpred <- predict(svm_bow_200, newdata = test_bow_200)
+svm_bow_200_train_accuracy <- mean(svm_bow_200_trainpred == train_bow_200$Pid)
+svm_bow_200_test_accuracy <- mean(svm_bow_200_testpred == test_bow_200$Pid)
 
 
-svm_bow_200_mcc <-  mcc(preds = as.numeric(predict(svm_bow_200, newdata = test_bow_200)),
+svm_bow_200_mcc <-  mcc(preds = as.numeric(svm_bow_200_testpred),
                         actuals = as.numeric(test_bow_200$Pid))
 
 ##############################################################################################################################
@@ -42,8 +44,10 @@ svm_US_bow_200 <- svm(Pid ~.,
 
 save(svm_US_bow_200, file = "Rdata/SVM_US_bow_200.Rdata")
 
-svm_US_bow_200_train_accuracy <- mean(predict(svm_US_bow_200, newdata = train_US_bow_200) == train_US_bow_200$Pid)
-svm_US_bow_200_test_accuracy <- mean(predict(svm_US_bow_200, newdata = test_US_bow_200) == test_US_bow_200$Pid)
+svm_US_bow_200_trainpred <- predict(svm_US_bow_200, newdata = train_US_bow_200)
+svm_US_bow_200_testpred <- predict(svm_US_bow_200, newdata = test_US_bow_200)
+svm_US_bow_200_train_accuracy <- mean(svm_US_bow_200_trainpred == train_US_bow_200$Pid)
+svm_US_bow_200_test_accuracy <- mean(svm_US_bow_200_testpred == test_US_bow_200$Pid)
 
 ##############################################################################################################################
 # SVM model on downsampled bag of words model -------------------------------
@@ -64,8 +68,10 @@ svm_DS_bow_200 <- svm(Pid ~.,
 
 save(svm_DS_bow_200, file = "Rdata/SVM_DS_bow_200.Rdata")
 
-svm_DS_bow_200_train_accuracy <- mean(predict(svm_DS_bow_200, newdata = train_DS_bow_200) == train_DS_bow_200$Pid)
-svm_DS_bow_200_test_accuracy <- mean(predict(svm_DS_bow_200, newdata = test_DS_bow_200) == test_DS_bow_200$Pid)
+svm_DS_bow_200_trainpred <- predict(svm_DS_bow_200, newdata = train_DS_bow_200)
+svm_DS_bow_200_testpred <- predict(svm_DS_bow_200, newdata = test_DS_bow_200)
+svm_DS_bow_200_train_accuracy <- mean(svm_DS_bow_200_trainpred == train_DS_bow_200$Pid)
+svm_DS_bow_200_test_accuracy <- mean(svm_DS_bow_200_testpred == test_DS_bow_200$Pid)
 
 ##############################################################################################################################
 # SVM model on original tfidf bag of words model -------------------------------
@@ -86,15 +92,17 @@ svm_tfidf_200 <- svm(Pid ~.,
 
 save(svm_tfidf_200, file = "Rdata/SVM_tfidf_200.Rdata")
 
-svm_tfidf_200_train_accuracy <- mean(predict(svm_tfidf_200, newdata = train_tfidf_200) == train_tfidf_200$Pid)
-svm_tfidf_200_test_accuracy <- mean(predict(svm_tfidf_200, newdata = test_tfidf_200) == test_tfidf_200$Pid)
+svm_tfidf_200_trainpred <- predict(svm_tfidf_200, newdata = train_tfidf_200)
+svm_tfidf_200_testpred <- predict(svm_tfidf_200, newdata = test_tfidf_200)
+svm_tfidf_200_train_accuracy <- mean(svm_tfidf_200_trainpred == train_tfidf_200$Pid)
+svm_tfidf_200_test_accuracy <- mean(svm_tfidf_200_testpred == test_tfidf_200$Pid)
 
 
-svm_tfidf_200_mcc <-  mcc(preds = as.numeric(predict(svm_tfidf_200, newdata = test_tfidf_200)),
+svm_tfidf_200_mcc <-  mcc(preds = as.numeric(svm_tfidf_200_testpred),
                         actuals = as.numeric(test_tfidf_200$Pid))
 
 ##############################################################################################################################
-# MLR model on upsampled tfidf bag of words model -------------------------------
+# SVM model on upsampled tfidf bag of words model -------------------------------
 # randomly splitting data
 set.seed(1)
 ind <- sample(1:nrow(US_tfidf_200), size = nrow(US_tfidf_200)*0.7, replace = F)
@@ -112,11 +120,13 @@ svm_US_tfidf_200 <- svm(Pid ~.,
 
 save(svm_US_tfidf_200, file = "Rdata/SVM_US_tfidf_200.Rdata")
 
-svm_US_tfidf_200_train_accuracy <- mean(predict(svm_US_tfidf_200, newdata = train_US_tfidf_200) == train_US_tfidf_200$Pid)
-svm_US_tfidf_200_test_accuracy <- mean(predict(svm_US_tfidf_200, newdata = test_US_tfidf_200) == test_US_tfidf_200$Pid)
+svm_US_tfidf_200_trainpred <- predict(svm_US_tfidf_200, newdata = train_US_tfidf_200)
+svm_US_tfidf_200_testpred <- predict(svm_US_tfidf_200, newdata = test_US_tfidf_200)
+svm_US_tfidf_200_train_accuracy <- mean(svm_US_tfidf_200_trainpred == train_US_tfidf_200$Pid)
+svm_US_tfidf_200_test_accuracy <- mean(svm_US_tfidf_200_testpred == test_US_tfidf_200$Pid)
 
 ##############################################################################################################################
-# MLR model on downsampled tfidf bag of words model -------------------------------
+# SVM model on downsampled tfidf bag of words model -------------------------------
 # randomly splitting data
 set.seed(1)
 ind <- sample(1:nrow(DS_tfidf_200), size = nrow(DS_tfidf_200)*0.7, replace = F)
@@ -134,5 +144,17 @@ svm_DS_tfidf_200 <- svm(Pid ~.,
 
 save(svm_DS_tfidf_200, file = "Rdata/SVM_DS_tfidf_200.Rdata")
 
-svm_DS_tfidf_200_train_accuracy <- mean(predict(svm_DS_tfidf_200, newdata = train_DS_tfidf_200) == train_DS_tfidf_200$Pid)
-svm_DS_tfidf_200_test_accuracy <- mean(predict(svm_DS_tfidf_200, newdata = test_DS_tfidf_200) == test_DS_tfidf_200$Pid)
+svm_DS_tfidf_200_trainpred <- predict(svm_DS_tfidf_200, newdata = train_DS_tfidf_200)
+svm_DS_tfidf_200_testpred <- predict(svm_DS_tfidf_200, newdata = test_DS_tfidf_200)
+svm_DS_tfidf_200_train_accuracy <- mean(svm_DS_tfidf_200_trainpred == train_DS_tfidf_200$Pid)
+svm_DS_tfidf_200_test_accuracy <- mean(svm_DS_tfidf_200_testpred == test_DS_tfidf_200$Pid)
+
+
+# save all predictions
+save(svm_bow_200_trainpred, svm_bow_200_testpred, 
+     svm_US_bow_200_trainpred, svm_US_bow_200_testpred, 
+     svm_DS_bow_200_trainpred, svm_DS_bow_200_testpred, 
+     svm_tfidf_200_trainpred, svm_tfidf_200_testpred,
+     svm_US_tfidf_200_trainpred, svm_US_tfidf_200_testpred,
+     svm_DS_tfidf_200_trainpred, svm_DS_tfidf_200_testpred, 
+     file = "Rdata/SVM_predictions.Rdata")
